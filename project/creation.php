ece36aa52e52d69b55c206a8127fc243c7a50573
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-04-30 21:25:01 +0200

    wip

--- project/creation.php
+++ project/creation.php
@@ -11,16 +11,28 @@ if(isset($_POST['envoyer'])){
 
 
   $requete = $bdd->prepare('insert into client (nom, prenom, genre, date_de_naissance, email, telephone) values (:nom, :prenom, :genre, :date_de_naissance, :email, :telephone)');
-  $requete->execute([
+  $requete->bindParam(':nom', $nom);
+  $requete->bindParam(':prenom', $prenom);
+  $requete->bindParam(':genre', $genre);
+  $requete->bindParam(':date_de_naissance', $date_de_naissance);
+  $requete->bindParam(':email', $email);
+  $requete->bindParam(':telephone', $telephone);
+
+
+  $succeed = $requete->execute();
+    /*[
     'nom' => $nom,
     'prenom' => $prenom,
     'genre' => $genre,
     'date_de_naissance' => $date_de_naissance,
     'email' => $email,
     'telephone' => $telephone,
-  ]);
-
-  print_r($requete->errorInfo());
+  ]*/
+  if($succeed){
+    echo "Enregistrement ok !";
+  } else {
+    print_r($requete->errorInfo());
+  }
 }
 
 ?>
