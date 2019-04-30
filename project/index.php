a3011160e62915038f1170a8ec86ef36701325d0
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-04-30 18:51:32 +0200

    add export sql

--- project/index.php
+++ project/index.php
@@ -4,6 +4,7 @@ try {
     $bdd = new PDO('mysql:host=127.0.0.1;dbname=agenda_en_ligne', 'root', 'root');
 } catch(\Exception $error){
   echo 'Impossible de se connecter à la BDD';
+  exit;
 }
 
 $requete = $bdd->prepare('select count(*) as nbre from client');
