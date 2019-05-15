6f8da601e06ab2e603509103c07fab62453a708f
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-05-15 21:16:36 +0200

    wip

--- project/formulaire.php
+++ project/formulaire.php
@@ -1,11 +1,9 @@
-<?= 'yooo'  ?>
-
 <form action="" method="post">
   nom <input type="text" name="nom" value="<?= $client['nom'] ?? '' ?>"><br>
-  prenom <input type="text" name="prenom" value=""><br>
-  genre <input type="text" name="genre" value=""><br>
-  Date de naissance <input type="text" name="date_de_naissance" value=""><br>
-  Email <input type="text" name="email" value=""><br>
-  Téléphone <input type="telephone" name="telephone" value=""><br>
+  prenom <input type="text" name="prenom" value="<?= $client['prenom'] ?? '' ?>"><br>
+  genre <input type="text" name="genre" value="<?= $client['genre'] ?? '' ?>"><br>
+  Date de naissance <input type="text" name="date_de_naissance" value="<?= $client['date_de_naissance'] ?? '' ?>"><br>
+  Email <input type="text" name="email" value="<?= $client['email'] ?? '' ?>"><br>
+  Téléphone <input type="telephone" name="telephone" value="<?= $client['telephone'] ?? '' ?>"><br>
   <input type="submit"  name="envoyer" value="Enregister">
 </form>
--- project/modifier.php
+++ project/modifier.php
@@ -1,5 +1,27 @@
 <?php
 require_once('connect.php');
+if(isset($_GET['id']) && $_POST){
+  $requeteSave = $bdd->prepare('update client set '.
+  'nom = :nom, prenom = :prenom, genre = :genre, date_de_naissance  = :date_de_naissance, '.
+  ' email = :email, telephone = :telephone where id =:id');
+  
+  $response = $requeteSave->execute([
+    'nom' => $_POST['nom'],
+    'prenom' => $_POST['prenom'],
+    'genre' => $_POST['genre'],
+    'date_de_naissance' => $_POST['date_de_naissance'],
+    'email' => $_POST['email'],
+    'telephone' => $_POST['telephone'],
+    'id' => $_GET['id'],
+  ]);
+  if($response){
+    header('location: index.php');
+    exit;
+  } else {
+    print_r($requeteSave->errorInfo());
+  }
+}
+
 
 if(isset($_GET['id'])) {
   $requete = $bdd->prepare("select * from client where id=:id");
