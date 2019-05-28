88d9a2fe49b7c28100254390d07564296b165e58
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-05-28 19:46:35 +0200

    wip

--- project/creation.php
+++ project/creation.php
@@ -4,11 +4,10 @@ require_once('connect.php');
 if(isset($_POST['envoyer'])){
   $nom = isset($_POST['nom']) ? $_POST['nom'] : '';
   $prenom = isset($_POST['prenom']) ? $_POST['prenom'] : '';
-  $genre = isset($_POST['genre']) ? $_POST['genre'] : null;
-  $date_de_naissance = isset($_POST['date_de_naissance']) ? $_POST['date_de_naissance'] : null;
+  $genre = isset($_POST['genre']) && !empty($_POST['genre']) ? $_POST['genre'] : null;
+  $date_de_naissance = isset($_POST['date_de_naissance']) && !empty($_POST['date_de_naissance']) ? $_POST['date_de_naissance'] : null;
   $email = isset($_POST['email']) ? $_POST['email'] : '';
   $telephone = isset($_POST['telephone']) ? $_POST['telephone'] : '';
-
 
   $requete = $bdd->prepare('insert into client (nom, prenom, genre, date_de_naissance, email, telephone) values (:nom, :prenom, :genre, :date_de_naissance, :email, :telephone)');
   $requete->bindParam(':nom', $nom);
--- project/formulaire.php
+++ project/formulaire.php
@@ -2,7 +2,7 @@
   nom <input type="text" name="nom" value="<?= $client['nom'] ?? '' ?>"><br>
   prenom <input type="text" name="prenom" value="<?= $client['prenom'] ?? '' ?>"><br>
   genre <input type="text" name="genre" value="<?= $client['genre'] ?? '' ?>"><br>
-  Date de naissance <input type="text" name="date_de_naissance" value="<?= $client['date_de_naissance'] ?? '' ?>"><br>
+  Date de naissance <input type="date" name="date_de_naissance" value="<?= $client['date_de_naissance'] ?? '' ?>"><br>
   Email <input type="text" name="email" value="<?= $client['email'] ?? '' ?>"><br>
   Téléphone <input type="telephone" name="telephone" value="<?= $client['telephone'] ?? '' ?>"><br>
   <input type="submit"  name="envoyer" value="Enregister">
